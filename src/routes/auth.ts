e388748b7e13ed950520cffc6ec895a6e335aa95
haikalgakbar <haikalgakbar@gmail.com>
2024-06-06 11:28:38 +0700

    add: routes

---
+++ src/routes/auth.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.post("/auth/login", {
+  /* TODO */
+});
+app.post("/auth/register", {
+  /* TODO */
+});
+app.post("/auth/reset", {
+  /* TODO */
+});
