061bd9dd1ceb8aab17453918c7439f0c6660e15c
haikalgakbar <haikalgakbar@gmail.com>
2024-06-09 20:47:52 +0700

    remove: middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth";
-import checkForEmptyBody from "./middlewares/checkForEmptyBody";
 
 const app = express();
-
-// app.use("/", checkForEmptyBody);
 
 app.use(express.json());
 app.use(authRoutes);
